f7812a316bf7921f3b1ad0edb0a1c91436c8fb7d
G00387859 <G00387859@gmit.ie>
2020-11-25 07:10:58 +0000

    update the mysql to create a table called store

--- Project/initDb.sql
+++ Project/initDb.sql
@@ -1,14 +1,11 @@
-/*productType == Durable :television, refrigerator, clothes, machines,
-                 Consumer :salt, pepper, soap, etc,
-                 Industrial Products : wheat, cotton, livestock, vegetables, fruits, - crude oil, fish, timber, metal.
-                 ref: https://www.businessmanagementideas.com/products/types-of-product/20612. 
-                 How reliable this? I dont know. 
-                  */
 use datarepresentation;
-CREATE TABLE store (
-    -> id int NOT NULL auto_increment,
-    -> productType VARCHAR(100),
-    -> productName varchar(100),
-    -> productPrice int(4),
-    -> PRIMARY KEY(id)
-    -> );
+    CREATE TABLE store (
+    id int NOT NULL auto_increment,
+    productType VARCHAR(100),
+    productName varchar(100),
+    productPrice float(4),
+    PRIMARY KEY(id),
+    UNIQUE KEY unique_productName (productName)
+);
+
+    
